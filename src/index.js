260fbab81f965996678cbef82ed8235a2d9d0a1a
Deeathwiing <andreykorotkins@gmail.com>
2020-10-13 19:57:51 +0300

    feat: complete Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 module.exports = 
 function reverse (n) {
   if(n < 0) { n*=-1};
-//   console.log(n);
- let arr= String(n).split('');
-  console.log(arr);
- arr = arr.reverse();
-  console.log(arr);
- 
+ let arr= String(n).split('').reverse();
  if (arr[arr.length] === 0) {
      delete arr[arr.lenght];
  }
