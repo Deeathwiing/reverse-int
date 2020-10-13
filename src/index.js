054ad43b819e47efa0944277b5f4656c8088973c
Deeathwiing <andreykorotkins@gmail.com>
2020-10-13 19:56:25 +0300

    init: Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = 
+function reverse (n) {
+  if(n < 0) { n*=-1};
+//   console.log(n);
+ let arr= String(n).split('');
+  console.log(arr);
+ arr = arr.reverse();
+  console.log(arr);
+ 
+ if (arr[arr.length] === 0) {
+     delete arr[arr.lenght];
+ }
+ return +arr.join('')
 }
+
+// console.log(reverse(-192));
